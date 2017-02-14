22eafbf47bbf600f182baa29a5c818afe79ba57c
JimmyMcAlias <notspelledlabby@gmail.com>
2017-02-14 11:24:33 -0600

    created MusicIF Interface :information_source:

---
+++ SimpleChat/src/simplechat/MusicIF.java
@@ -0,0 +1,15 @@
+package simplechat;
+
+
+
+
+
+
+public interface MusicIF 
+{
+  /**
+   * Method that when overriden is used to display objects onto
+   * a UI.
+   */
+  public abstract void sing(String notePlayed);
+}
